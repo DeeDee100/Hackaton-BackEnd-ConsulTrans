64f8b0e7a358331fe68d60014a189e244857c295
DeeDee <amanda_tinoco00@hotmail.com>
2021-11-27 20:59:10 -0300

    fix: Mudança nos campos do banco de dados

--- database/models.py
+++ database/models.py
@@ -10,14 +10,17 @@ from sqlalchemy.orm import relationship
 class Medicos(Base):
 	__tablename__ = "medicos"
 
-	id = Column(Integer, primary_key=True, onupdate="CASCADE")
+	id = Column(Integer, primary_key=True)
 	crm = Column(String, unique=True)
 	email = Column(String, unique=True)
+	password = Column(String, nullable=False)
 	name = Column(String, nullable=False)
-	estado = Column(String, nullable=True)
+	sobrenome = Column(String, nullable=False)
+	instagram = Column(String, nullable=True)
 	especialidade = Column(String, nullable=False)
 	endereco_principal = Column(String, nullable=False)
-	endereco_opcional = Column(String, nullable=True)
+	site = Column(String, nullable=False)
+	atendimento = Column(String, nullable=False)
 	pcd = Column(Boolean, nullable= False)
 	descript = Column(String, nullable=True)
 	phone = Column(Integer, nullable=False)
